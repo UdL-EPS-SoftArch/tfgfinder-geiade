178ed9f177002c09481a5c2de28b30aa53eaeb4f
Àngela Giribet <angelagiribet@gmail.com>
2025-05-07 17:40:19 +0200

    Add component Invite and corresponding route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserRegisterComponent } from './user/user-register/user-register.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
 import { UserDeleteComponent } from './user/user-delete/user-delete.component';
+import { InviteComponent} from "./invite/invite.component";
 
 const routes: Routes = [
   { path: 'users/create', component: UserRegisterComponent},
@@ -18,6 +19,8 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent},
   { path: '404', component: NotFoundComponent},
   { path: '', redirectTo: 'about', pathMatch: 'full'},
+
+  { path: 'invite', component: InviteComponent, canActivate: [LoggedInGuard]},
 ];
 
 @NgModule({
---
+++ src/app/invite/invite.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invite',
+  imports: [],
+  templateUrl: './invite.component.html'
+})
+export class InviteComponent {
+
+}
