3620c248befaa7267d1cdcb9b2f56a50f496d1a0
Isaac Torres Espuña <isaac@ite2002.net>
2025-05-14 18:59:20 +0200

    Created proposal.ts resource

--- src/app/proposal/proposal.ts
+++ src/app/proposal/proposal.ts
@@ -1,3 +1,20 @@
-export class Proposal {
+import {HateoasResource, Resource} from "@lagoshny/ngx-hateoas-client";
+import {User} from "../login-basic/user";
 
+@HateoasResource('proposals')
+export class Proposal extends Resource{
+  id: number;
+  title: string;
+  description: string;
+  timing: string;
+  speciality: string;
+  kind: string;
+  keywords: string[];
+  user: User;
+  category: string[] = []; //TODO: Pending Category Implementation
+
+  constructor(values: object = {}) {
+    super();
+    Object.assign(this as any, values);
+  }
 }
