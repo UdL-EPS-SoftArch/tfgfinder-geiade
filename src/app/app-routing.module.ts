fc2b1f27ddece62c4a109e9fede7ccbc8219bf83
Núria <nurial3012@gmail.com>
2025-05-14 19:15:40 +0200

    Show category list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,8 @@ import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserRegisterComponent } from './user/user-register/user-register.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
 import { UserDeleteComponent } from './user/user-delete/user-delete.component';
+import {CategoryListComponent} from "./category/category-list/category-list.component";
+
 
 const routes: Routes = [
   { path: 'users/create', component: UserRegisterComponent},
@@ -18,6 +20,7 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent},
   { path: '404', component: NotFoundComponent},
   { path: '', redirectTo: 'about', pathMatch: 'full'},
+  { path: 'categories', component: CategoryListComponent},
 ];
 
 @NgModule({
---
+++ src/app/category/category-list/category-list.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Category } from "../category";
+import { CategoryService } from "../category.service";
+import { PagedResourceCollection} from "@lagoshny/ngx-hateoas-client";
+import {NgbPagination} from "@ng-bootstrap/ng-bootstrap";
+import {NgForOf} from "@angular/common";
+
+
+@Component({
+  selector: 'app-category-list',
+  imports: [
+    NgbPagination, NgForOf
+  ],
+  templateUrl: './category-list.component.html'
+})
+export class CategoryListComponent implements OnInit {
+  public categories: Category[] = [];
+  public pageSize = 5;
+  public page = 1;
+  public totalcategories = 0;
+
+  constructor(
+    private categoryService: CategoryService) {
+  }
+
+  ngOnInit(): void {
+    this.categoryService.getPage({ pageParams:  { size: this.pageSize } }).subscribe(
+      (page: PagedResourceCollection<Category>) => {
+        this.categories = page.resources;
+        this.totalcategories = page.totalElements;
+      });
+  }
+
+  changePage(): void {
+    this.categoryService.getPage({ pageParams: { page: this.page - 1, size: this.pageSize }}).subscribe(
+      (page: PagedResourceCollection<Category>) => this.categories = page.resources);
+  }
+
+  detail(event: any): void {
+    this.categories = event.result.category;
+    this.totalcategories = event.result.totalElements;
+  }
+}
+
+
+
+
