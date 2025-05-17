3a6d6ac92db0de50ca10ea5c7ef6360608730b62
BielGuallar <bielguallar@gmail.com>
2025-05-17 21:17:53 +0200

    added all the correct files to the new branch so the register works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,16 +8,30 @@ import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserRegisterComponent } from './user/user-register/user-register.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
 import { UserDeleteComponent } from './user/user-delete/user-delete.component';
+import { OrganisationRegisterComponent } from './organisation/organisation-register/organisation-register.component';
+import { RegisterTypeSelectionComponent } from './register-type-selection/register-type-selection.component';
+import { StudentRegisterComponent } from './student/student-register/student-register.component';
+import { ProfessorRegisterComponent } from './professor/professor-register/professor-register.component';
+//import { ExternalListComponent } from './admin/external-list/external-list.component';
+
+
 
 const routes: Routes = [
-  { path: 'users/create', component: UserRegisterComponent},
+  { path: 'users/create', redirectTo: 'register-student', pathMatch: 'full'},
+  //{ path: 'users/create', component: UserRegisterComponent},
   { path: 'users/:id/delete', component: UserDeleteComponent, canActivate: [LoggedInGuard]},
   { path: 'users/:id/edit', component: UserEditComponent, canActivate: [LoggedInGuard]},
   { path: 'users/:id', component: UserDetailComponent, canActivate: [LoggedInGuard]},
   { path: 'users', component: UserListComponent, canActivate: [LoggedInGuard]},
+  {path: 'register-organisation', component: OrganisationRegisterComponent},
+  { path: 'register', component: RegisterTypeSelectionComponent },
+  { path: 'register-student', component: StudentRegisterComponent },
+  { path: 'register-professor', component: ProfessorRegisterComponent },
+  //{ path: 'admin/organisations', component: ExternalListComponent },
   { path: 'about', component: AboutComponent},
   { path: '404', component: NotFoundComponent},
   { path: '', redirectTo: 'about', pathMatch: 'full'},
+
 ];
 
 @NgModule({
