bc469ffbaf9b13f69f00851f49f6f06ed0468b0e
Núria <nurial3012@gmail.com>
2025-05-17 15:09:20 +0200

    Added button to create new category

--- src/app/category/category-register/category-register.component.ts
+++ src/app/category/category-register/category-register.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {FormsModule} from "@angular/forms";
-
 import {ActivatedRoute, Router} from '@angular/router';
 import { Location } from '@angular/common';
 import {Category} from "../category";
