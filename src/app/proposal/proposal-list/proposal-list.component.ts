2cff89477a45af3a2ea4f455dd612f72dd419e93
Isaac Torres Espuña <isaac@ite2002.net>
2025-05-17 22:39:20 +0200

    Retrieve category on proposal load

--- src/app/proposal/proposal-list/proposal-list.component.ts
+++ src/app/proposal/proposal-list/proposal-list.component.ts
@@ -2,12 +2,12 @@ import { Router, RouterLink } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { PagedResourceCollection } from '@lagoshny/ngx-hateoas-client';
 import { NgbPagination } from '@ng-bootstrap/ng-bootstrap';
-import { NgForOf } from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 import { ProposalService} from "../proposal.service";
 import {Proposal} from "../proposal";
 
 @Component({
-  imports: [RouterLink, NgbPagination, NgForOf],
+  imports: [RouterLink, NgbPagination, NgForOf, NgIf],
   selector: 'app-proposals-list',
   templateUrl: './proposal-list.component.html'
 })
@@ -23,17 +23,28 @@ export class ProposalListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.proposalService.getPage({ pageParams:  { size: this.pageSize }, sort: { title: 'ASC' } }).subscribe(
+    this.proposalService.getPage({ pageParams: { size: this.pageSize }, sort: { title: 'ASC' } }).subscribe(
       (page: PagedResourceCollection<Proposal>) => {
-        this.proposals = page.resources;
         this.totalProposals = page.totalElements;
+        this.proposalService.loadCategoriesForProposals(page.resources).subscribe(
+          (proposalsWithCategories: Proposal[]) => {
+            this.proposals = proposalsWithCategories;
+          }
+        );
       });
   }
 
   changePage(): void {
     this.proposalService.getPage({ pageParams: { page: this.page - 1, size: this.pageSize }, sort: { username: 'ASC' } }).subscribe(
-      (page: PagedResourceCollection<Proposal>) => this.proposals = page.resources);
+      (page: PagedResourceCollection<Proposal>) => {
+        this.proposalService.loadCategoriesForProposals(page.resources).subscribe(
+          (proposalsWithCategories: Proposal[]) => {
+            this.proposals = proposalsWithCategories;
+          }
+        );
+      });
   }
+
 
   detail(proposal: Proposal): void {
     this.router.navigate(['proposals', proposal.id]);
